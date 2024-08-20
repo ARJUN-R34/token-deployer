8b2642448acd770b7b71affff99ec5ceb3964650
Arjun Sasikumar <itsarjn@gmail.com>
2024-08-20 18:28:46 +0530

    Updated github url

--- src/pages/deployer/index.tsx
+++ src/pages/deployer/index.tsx
@@ -189,11 +189,11 @@ function Description() {
           available here
         </Link>
         . The HTML form is also{" "}
-        <Link target="_blank" href="https://github.com/ton-blockchain/minter">
+        <Link target="_blank" href="https://github.com/arjun-r34/token-deployer">
           open source
         </Link>{" "}
         and served from{" "}
-        <Link target="_blank" href="https://github.com/ton-blockchain/minter">
+        <Link target="_blank" href="https://github.com/arjun-r34/token-deployer">
           GitHub Pages
         </Link>
         . <Spacer />
